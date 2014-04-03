ee26e65ba3df8197598db5621bb0dfc7e269e58d
Prelang Builder <builder@prelang.com>
2014-04-03 22:52:45 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReunionHs::Application.config.secret_key_base = '1aa6c1823541ecc80f9334a065316015b5a622f41b956104b46da7cf5d879d9f9d213ef552ccacc1fefd7642c8259f7dfd4ed7c2a946014263932b967cebafde'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReunionHs::Application.config.session_store :cookie_store, key: '_reunion_hs_session'
